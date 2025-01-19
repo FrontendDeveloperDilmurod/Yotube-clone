6a6ab843bad8da0715de1c8da1a6a0d14e8d923f
Dilmurod Mamurov <mamurovd426@gmail.com>
2025-01-19 17:05:18 +0500

    Feed component

--- src/components/Sidibar.jsx
+++ src/components/Sidibar.jsx
@@ -19,134 +19,84 @@ const Sidibar = () => {
 
   return (
     <div
-      // className={` mt-16 pb-12 top-0 left-0 h-screen p-4  transition-transform transform ${sidebar ? 'w-60' : 'w-20'}`}
-      className={`mt-16 pb-12 top-16 left-0 h-screen p-4 overflow-y-auto transition-transform transform sticky ${sidebar ? "w-60" : "w-20"
-        } hover:scrollbar-thin hover:scrollbar-thumb-gray-400 hover:scrollbar-track-gray-100`}
+      className={`mt-16 pb-12 top-16 left-0 h-screen p-4 overflow-y-auto transition-transform transform sticky ${sidebar ? "w-60" : "w-20"} hover:scrollbar-thin hover:scrollbar-thumb-gray-400 hover:scrollbar-track-gray-100`}
     >
       <div
-        className={`flex gap-4 items-center p-1 rounded-lg cursor-pointer  hover:bg-gray-200 first-line:${category === 12 ? " bg-gray-300" : ""
-          }`}
+        className={`flex gap-4 items-center p-1 rounded-lg cursor-pointer hover:bg-gray-200 ${category === 12 ? " bg-gray-300" : ""}`}
         onClick={() => setCategory(12)}
       >
-        <Link
-          to="/"
-          className="flex items-center gap-4"
-          activeClassName="bg-gray-300" // Change this to your active class style
-        >
-          <img src={home} alt="home" className={`w-${sidebar ? "6" : "6"}`} />
+        <Link to="/" className="flex items-center gap-4">
+          <img src={home} alt="home" className={`w-${sidebar ? "6" : "8"} sm:w-8 md:w-8`} />
           {sidebar && <p className="text-base font-semibold">Home</p>}
         </Link>
       </div>
       <div
-        className={`flex gap-4 items-center p-1 mt-3 rounded-lg cursor-pointer hover:bg-gray-200 ${category === 31 ? " bg-gray-300" : ""
-          }`}
+        className={`flex gap-4 items-center p-1 mt-3 rounded-lg cursor-pointer hover:bg-gray-200 ${category === 31 ? " bg-gray-300" : ""}`}
         onClick={() => setCategory(31)}
       >
-        <img
-          src={game_icon}
-          alt="game_icon"
-          className={`w-${sidebar ? "6" : "8"}`}
-        />
+        <img src={game_icon} alt="game_icon" className={`w-${sidebar ? "6" : "8"} sm:w-8 md:w-8`} />
         {sidebar && <p className="text-base font-semibold">Gaming</p>}
       </div>
       <div
-        className={`flex gap-4 items-center p-1 mt-3 rounded-lg cursor-pointer hover:bg-gray-200 ${category === 21 ? " bg-gray-300" : ""
-          }`}
+        className={`flex gap-4 items-center p-1 mt-3 rounded-lg cursor-pointer hover:bg-gray-200 ${category === 21 ? " bg-gray-300" : ""}`}
         onClick={() => setCategory(21)}
       >
-        <img
-          src={automobiles}
-          alt="game_icon"
-          className={`w-${sidebar ? "6" : "8"}`}
-        />
+        <img src={automobiles} alt="automobiles" className={`w-${sidebar ? "6" : "8"} sm:w-8 md:w-8`} />
         {sidebar && <p className="text-base font-semibold">Automobiles</p>}
       </div>
       <div
-        className={`flex gap-4 items-center p-1 mt-3 rounded-lg cursor-pointer hover:bg-gray-200 ${category === 13 ? " bg-gray-300" : ""
-          }`}
+        className={`flex gap-4 items-center p-1 mt-3 rounded-lg cursor-pointer hover:bg-gray-200 ${category === 13 ? " bg-gray-300" : ""}`}
         onClick={() => setCategory(13)}
       >
-        <img
-          src={sports}
-          alt="game_icon"
-          className={`w-${sidebar ? "6" : "8"}`}
-        />
+        <img src={sports} alt="sports" className={`w-${sidebar ? "6" : "8"} sm:w-8 md:w-8`} />
         {sidebar && <p className="text-base font-semibold">Sports</p>}
       </div>
       <div
-        className={`flex gap-4 items-center p-1 mt-3 rounded-lg cursor-pointer hover:bg-gray-200 ${category === 22 ? " bg-gray-300" : ""
-          }`}
+        className={`flex gap-4 items-center p-1 mt-3 rounded-lg cursor-pointer hover:bg-gray-200 ${category === 22 ? " bg-gray-300" : ""}`}
         onClick={() => setCategory(22)}
       >
-        <img
-          src={entertamen}
-          alt="game_icon"
-          className={`w-${sidebar ? "6" : "8"}`}
-        />
+        <img src={entertamen} alt="entertainment" className={`w-${sidebar ? "6" : "8"} sm:w-8 md:w-8`} />
         {sidebar && <p className="text-base font-semibold">Entertainment</p>}
       </div>
       <div
-        className={`flex gap-4 items-center p-1 mt-3 rounded-lg cursor-pointer hover:bg-gray-200 ${category === 11 ? " bg-gray-300" : ""
-          }`}
+        className={`flex gap-4 items-center p-1 mt-3 rounded-lg cursor-pointer hover:bg-gray-200 ${category === 11 ? " bg-gray-300" : ""}`}
         onClick={() => setCategory(11)}
       >
-        <img
-          src={teach}
-          alt="game_icon"
-          className={`w-${sidebar ? "6" : "8"}`}
-        />
+        <img src={teach} alt="technology" className={`w-${sidebar ? "6" : "8"} sm:w-8 md:w-8`} />
         {sidebar && <p className="text-base font-semibold">Technology</p>}
       </div>
       <div
-        className={`flex gap-4 items-center p-1 mt-3 rounded-lg cursor-pointer hover:bg-gray-200 ${category === 4 ? " bg-gray-300" : ""
-          }`}
+        className={`flex gap-4 items-center p-1 mt-3 rounded-lg cursor-pointer hover:bg-gray-200 ${category === 4 ? " bg-gray-300" : ""}`}
         onClick={() => setCategory(4)}
       >
-        <img
-          src={music}
-          alt="game_icon"
-          className={`w-${sidebar ? "6" : "8"}`}
-        />
+        <img src={music} alt="music" className={`w-${sidebar ? "6" : "8"} sm:w-8 md:w-8`} />
         {sidebar && <p className="text-base font-semibold">Music</p>}
       </div>
       <div
-        className={`flex gap-4 items-center p-1 mt-3 rounded-lg cursor-pointer hover:bg-gray-200 ${category === 3 ? " bg-gray-300" : ""
-          }`}
+        className={`flex gap-4 items-center p-1 mt-3 rounded-lg cursor-pointer hover:bg-gray-200 ${category === 3 ? " bg-gray-300" : ""}`}
         onClick={() => setCategory(3)}
       >
-        <img
-          src={blogs}
-          alt="game_icon"
-          className={`w-${sidebar ? "6" : "8"}`}
-        />
+        <img src={blogs} alt="blogs" className={`w-${sidebar ? "6" : "8"} sm:w-8 md:w-8`} />
         {sidebar && <p className="text-base font-semibold">Blogs</p>}
       </div>
-
       <div
-        className={`flex gap-4 items-center p-1 mt-3 rounded-lg cursor-pointer hover:bg-gray-200 ${category === 26 ? " bg-gray-300" : ""
-          }`}
+        className={`flex gap-4 items-center p-1 mt-3 rounded-lg cursor-pointer hover:bg-gray-200 ${category === 26 ? " bg-gray-300" : ""}`}
         onClick={() => setCategory(26)}
       >
-        <img
-          src={news}
-          alt="game_icon"
-          className={`w-${sidebar ? "6" : "8"}`}
-        />
+        <img src={news} alt="news" className={`w-${sidebar ? "6" : "8"} sm:w-8 md:w-8`} />
         {sidebar && <p className="text-base font-semibold">News</p>}
       </div>
-      {/* Qo'shimcha bo'limlar */}
+
+      {/* Suggested Channels */}
       <div className="mt-8">
         {sidebar && <p className="text-base font-bold">Suggested Channels</p>}
         <div className="mt-4">
           <Link to={'/channnels'}>
-            <div className={`flex gap-4 items-center p-1 mt-3 rounded-lg cursor-pointer hover:bg-gray-200 ${category === 262 ? " bg-gray-300" : ""
-              }`}
-              onClick={() => setCategory(262)}>
-              <img
-                src={developer}
-                alt="jack"
-                className="w-10 h-10 rounded-full"
-              />
+            <div
+              className={`flex gap-4 items-center p-1 mt-3 rounded-lg cursor-pointer hover:bg-gray-200 ${category === 262 ? " bg-gray-300" : ""}`}
+              onClick={() => setCategory(262)}
+            >
+              <img src={developer} alt="developer" className="w-10 h-10 rounded-full" />
               {sidebar && <p className="text-base font-semibold">Frontend_Developer</p>}
             </div>
           </Link>
